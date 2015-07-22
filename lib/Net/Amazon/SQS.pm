d89562592f43c809d2b1215f247718a5a9ff910c
Kazuhiro Shibuya <shibuya@fout.jp>
2015-07-22 17:34:19 +0900

    Add documents

--- lib/Net/Amazon/SQS.pm
+++ lib/Net/Amazon/SQS.pm
@@ -387,15 +387,281 @@ __END__
 
 =head1 NAME
 
-Net::Amazon::SQS - It's new $module
+Net::Amazon::SQS - Amazon SQS client
 
 =head1 SYNOPSIS
 
     use Net::Amazon::SQS;
 
+    my $sqs = Net::Amazon::SQS->new(
+        access_key => "XXXXX",
+        secret_key => "YYYYY",
+        region => "ap-northeast-1",
+    );
+    my %queue = $sqs->list_queues->{ListQueueResult};
+
 =head1 DESCRIPTION
 
-Net::Amazon::SQS is ...
+Net::Amazon::SQS is simple Amazon SQS simple client.
+
+THIS IS A DEVELOPMENT RELEASE. API MAY CHANGE WITHOUT NOTICE.
+
+=head1 METHODS
+
+=head2 add_permission
+
+Adds a permission to a queue for a specific principal.
+
+    $sqs->add_permission({
+        "AWSAccountId.1" => "12345678",
+        "ActionName.1" => "SendMessage",
+        QueueUrl => "http://localhost:9324/queue/test_queue",
+        Label => "testLabel"
+    });
+
+=over 4
+
+=item * SEE L<http://docs.aws.amazon.com/ja_jp/AWSSimpleQueueService/latest/APIReference/API_AddPermission.html>
+
+=back
+
+=head2 change_message_visibility
+
+Changes the visibility timeout of a specified message in a queue to a new value.
+
+    $sqs->change_message_visibility({
+        QueueUrl => "http://localhost:9324/queue/test_queue",
+        ReceiptHandle => $res->{ReceiveMessageResult}->{Message}->{ReceiptHandle},
+        VisibilityTimeout => 60
+    });
+
+=over 4
+
+=item * SEE L<http://docs.aws.amazon.com/ja_jp/AWSSimpleQueueService/latest/APIReference/API_ChangeMessageVisibility.html>
+
+=back
+
+=head2 change_message_visibility_bacth
+
+Changes the visibility timeout of multiple messages.
+
+    $sqs->change_message_visibility_batch({
+        "ChangeMessageVisibilityBatchRequestEntry.1.Id" => "change_visibility_msg_2",
+        "ChangeMessageVisibilityBatchRequestEntry.1.ReceiptHandle" => "gfk0T0R0waama4fVFffkjKzmhMCymjQvfTFk2LxT33G4ms5subrE0deLKWSscPU1oD3J9zgeS4PQQ3U30qOumIE6AdAv3w%2F%2Fa1IXW6AqaWhGsEPaLm3Vf6IiWqdM8u5imB%2BNTwj3tQRzOWdTOePjOjPcTpRxBtXix%2BEvwJOZUma9wabv%2BSw6ZHjwmNcVDx8dZXJhVp16Bksiox%2FGrUvrVTCJRTWTLc59oHLLF8sEkKzRmGNzTDGTiV%2BYjHfQj60FD3rVaXmzTsoNxRhKJ72uIHVMGVQiAGgBX6HGv9LDmYhPXw4hy%2FNgIg%3D%3D",
+        "ChangeMessageVisibilityBatchRequestEntry.1.VisibilityTimeout" => 45,
+        QueueUrl => "http://localhost:9324/queue/test_queue",
+    });
+
+=over 4
+
+=item * SEE L<http://docs.aws.amazon.com/ja_jp/AWSSimpleQueueService/latest/APIReference/API_ChangeMessageVisibilityBatch.html>
+
+=back
+
+=head2 create_queue
+
+Create a new queue, or returns the URL of an existing one.
+
+    $sqs->create_queue({
+        QueueName => "test_queue"
+    });
+
+=over 4
+
+=item * SEE L<http://docs.aws.amazon.com/ja_jp/AWSSimpleQueueService/latest/APIReference/API_CreateQueue.html>
+
+=back
+
+=head2 delete_message
+
+Deletes the specified message from the specified queue.
+
+    $sqs->delete_message({
+        QueueUrl => "http://localhost:9324/queue/test_queue",
+        ReceiptHandle => $res->{ReceiveMessageResult}->{Message}->{ReceiptHandle},
+        VisibilityTimeout => 60
+    });
+
+=over 4
+
+=item * SEE L<http://docs.aws.amazon.com/ja_jp/AWSSimpleQueueService/latest/APIReference/API_DeleteMessage.html>
+
+=back
+
+=head2 delete_message_batch
+
+Deletes up to ten messages from the specified queue.
+
+    $sqs->delete_message_batch({
+        QueueUrl => "http://localhost:9324/queue/test_queue",
+        "DeleteMessageBatchRequestEntry.1.Id" => "msg1",
+        "DeleteMessageBatchRequestEntry.1.ReceiptHandle" => $res->{ReceiveMessageResult}->{Message}->{ReceiptHandle},
+    });
+
+=over 4
+
+=item * SEE L<http://docs.aws.amazon.com/ja_jp/AWSSimpleQueueService/latest/APIReference/API_DeleteMessageBatch.html>
+
+=back
+
+=head2 delete_queue
+
+Deletes the queue specified by the queue URL.
+
+    $sqs->delete_queue({
+        QueueUrl => "http://localhost:9324/queue/test_queue"
+    });
+
+=over 4
+
+=item * SEE L<http://docs.aws.amazon.com/ja_jp/AWSSimpleQueueService/latest/APIReference/API_DeleteQueue.html>
+
+=back
+
+=head2 get_queue_attributes
+
+Gets attributes for the specified queue.
+
+    $sqs->get_queue_attributes({
+        QueueUrl => "http://localhost:9324/queue/test_queue",
+        "AttributeName.1" => "VisibilityTimeout",
+    });
+
+=over 4
+
+=item * SEE L<http://docs.aws.amazon.com/ja_jp/AWSSimpleQueueService/latest/APIReference/API_GetQueueAttributes.html>
+
+=back
+
+=head2 get_queue_url
+
+Returns the URL of an existing queue.
+
+    $sqs->get_queue_url({
+        QueueName => "test_queue",
+    });
+
+=over 4
+
+=item * SEE L<http://docs.aws.amazon.com/ja_jp/AWSSimpleQueueService/latest/APIReference/API_GetQueueUrl.html>
+
+=back
+
+=head2 list_dead_letter_source_queues
+
+Returns a list of your queues that have the Redrive Policy queue attribute configured with a dead letter queue.
+
+    $sqs->list_dead_letter_source_queues({
+        QueueUrl => "http://localhost:9324/queue/test_queue",
+    });
+
+=over 4
+
+=item * SEE L<http://docs.aws.amazon.com/ja_jp/AWSSimpleQueueService/latest/APIReference/API_ListDeadLetterSourceQueues.html>
+
+=back
+
+=head2 list_queues
+
+Returns a list of you queues.
+
+    $sqs->list_queues;
+
+=over 4
+
+=item * SEE L<http://docs.aws.amazon.com/ja_jp/AWSSimpleQueueService/latest/APIReference/API_ListQueues.html>
+
+=back
+
+=head2 purge_queue
+
+Deletes the messages in a queue specified by the queue URL.
+
+    $sqs->purge_queue({
+        QueueUrl => "http://localhost:9324/queue/test_queue",
+    });
+
+=over 4
+
+=item * SEE L<http://docs.aws.amazon.com/ja_jp/AWSSimpleQueueService/latest/APIReference/API_PurgeQueue.html>
+
+=back
+
+=head2 receive_message
+
+Retrieves one or more messages, with a maximum limit of 10 messages, from the specified queue.
+
+    $sqs->receive_message({
+        QueueUrl => "http://localhost:9324/queue/test_queue",
+    });
+
+=over 4
+
+=item * SEE L<http://docs.aws.amazon.com/ja_jp/AWSSimpleQueueService/latest/APIReference/API_ReceiveMessage.html>
+
+=back
+
+=head2 remove_permission
+
+Revokes any permissions in the queue policy that matches the specified Lable parameter.
+
+    $sqs->remove_permission({
+        Label => "testLabel"
+        QueueUrl => "http://localhost:9324/queue/test_queue",
+    });
+
+=over 4
+
+=item * SEE L<http://docs.aws.amazon.com/ja_jp/AWSSimpleQueueService/latest/APIReference/API_RemovePermission.html>
+
+=back
+
+=head2 send_message
+
+Delivers a message to the specified queue.
+
+    $sqs->send_message({
+        QueueUrl => "http://localhost:9324/queue/test_queue",
+        MessageBody => "Hello!"
+    });
+
+=over 4
+
+=item * SEE L<http://docs.aws.amazon.com/ja_jp/AWSSimpleQueueService/latest/APIReference/API_SendMessage.html>
+
+=back
+
+=head2 send_message_batch
+
+Delivers up to ten messages to the specified queue.
+
+    $sqs->send_message_batch({
+        "SendMessageBatchRequestEntry.1.Id" => "msg1",
+        "SendMessageBatchRequestEntry.1.MessageBody" => "Hello!",
+        QueueUrl => "http://localhost:9324/queue/test_queue",
+    });
+
+=over 4
+
+=item * SEE L<http://docs.aws.amazon.com/ja_jp/AWSSimpleQueueService/latest/APIReference/API_SendMessageBatch.html>
+
+=back
+
+=head2 set_queue_attributes
+
+Sets the value of one or more queue attributes.
+
+    $sqs->set_queue_attributes({
+        QueueUrl => "http://localhost:9324/queue/test_queue",
+        "Attribute.Name" => "VisibilityTimeout",
+        "Attribute.Value" => 40,
+    });
+
+=over 4
+
+=item * SEE L<http://docs.aws.amazon.com/ja_jp/AWSSimpleQueueService/latest/APIReference/API_SetQueueAttributes.html>
+
+=back
 
 =head1 LICENSE
 
